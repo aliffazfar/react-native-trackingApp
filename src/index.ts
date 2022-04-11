685a3761febff4274e201e64eb1796da3d31b096
aliff.azfar <aliff.azfar@aleph-labs.com>
2022-04-11 13:45:21 +0800

    setup mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
+
+const mongoUri =
+  'mongodb+srv://john:1234@nodeexpressproject.zkoxj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri)
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err)
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi there!')
