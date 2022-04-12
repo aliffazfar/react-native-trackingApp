f02b7a34a43c47d7e740e2ae9b4aa8e61f51b825
aliff.azfar <aliff.azfar@aleph-labs.com>
2022-04-12 12:00:54 +0800

    salting and hashing

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -11,5 +12,52 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 })
+
+userSchema.pre('save', function (next) {
+  const user = this
+  if (!user.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.genSalt(10, (err: mongoose.CallbackError | undefined, salt: any) => {
+    if (err) {
+      return next(err)
+    }
+
+    bcrypt.hash(
+      user.password,
+      salt,
+      (err: mongoose.CallbackError | undefined, hash: any) => {
+        if (err) {
+          return next(err)
+        }
+        user.password = hash
+        next()
+      }
+    )
+  })
+})
+
+userSchema.methods.comparePassword = function (candidatePassword: any) {
+  const user = this
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(
+      candidatePassword,
+      user.password,
+      (err: any, isMatch: any) => {
+        if (err) {
+          return reject(err)
+        }
+
+        if (!isMatch) {
+          return reject(false)
+        }
+
+        resolve(true)
+      }
+    )
+  })
+}
 
 mongoose.model('User', userSchema)
